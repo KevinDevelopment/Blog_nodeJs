cdf07e818c839284765d024af6d39f27ca97b121
Kevin Ferreira <kevincristhian@outlook.com>
2022-03-09 19:08:56 -0300

    model com banco de dados adicionado

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 const handlebars = require('express-handlebars')
 const bodyParser = require('body-parser')
-const mongoose = ('mongoose')
+const mongoose = require('mongoose')
 const admin = require('./routes/admin')
 const path = require('path')
 
@@ -19,9 +19,12 @@ app.engine('handlebars', handlebars.engine())
 app.set('view engine', 'handlebars')
 app.set('views', __dirname + '/views');
 
-
-
 //mongoose
+mongoose.connect("mongodb://localhost/blogapp").then(() => {
+    console.log("conexão ao banco estabelecida com sucesso!")
+}).catch(err => {
+    console.log(`não foi possivel se concetar ai banco de dados ${err}`)
+})
 
 //public 
 app.use(express.static(path.join(__dirname, 'public')))
