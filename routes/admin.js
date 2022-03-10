530d1ef466ffcd7392df91d99a0b18d0beb81700
Kevin Ferreira <kevincristhian@outlook.com>
2022-03-10 18:52:32 -0300

    adicionado banco de dados

--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,10 @@
-const { response } = require('express');
-const express = require('express')
+const express = require('express');
 const router = express.Router()
 const path = require('path')
 const app = express()
+const mongoose = require('mongoose')
+require('../models/Categoria')
+const categoria = mongoose.model('categorias')
 app.set('views', path.join(__dirname, '../views'));
 
 
@@ -17,6 +19,19 @@ router.get('/posts', (require, response) => {
 
 router.get('/categorias', (require, response) => {
     response.render('admin/categorias')
+})
+
+router.post('/categorias/nova', (require, response) => {
+    const novaCategoria = {
+        nome: require.body.nome,
+        slug: require.body.slug
+    }
+
+    new categoria(novaCategoria).save().then(() => {
+        console.log("Categoria salva com sucesso!")
+    }).catch(err => {
+        console.log("erro ao salvar categoria")
+    })
 })
 
 router.get('/categorias/add', (require, response) => {
